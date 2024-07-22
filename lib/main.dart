d260630d3bcfbf803272f7fa527aba8bd2d61d0f
TreNgheDiCode <gabayan170@gmail.com>
2024-07-23 03:11:25 +0700

    Add Cloud Messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final notificationSettings =
+      await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+// For apple platforms, ensure the APNS token is available before making any FCM plugin API calls
+  final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+  if (apnsToken != null) {
+    // APNS token is available, make FCM plugin API requests...
+  }
+
+  // Log the token
+  print('FCM Token: $fcmToken');
+
+  // For web platforms, the `pushNotification` event is not supported. Use `onMessage` and `onBackgroundMessage` instead.
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   print('Got a message whilst in the foreground!');
+  //   print('Message data: ${message.data}');
+
+  //   if (message.notification != null) {
+  //     print('Message also contained a notification: ${message.notification}');
+  //   }
+  // });
+
   runApp(const MainApp());
 }
 
@@ -9,11 +41,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Initialization Example'),
+      ),
+      body: const Center(
+        child: Text('Firebase Initialized Successfully'),
       ),
     );
   }
